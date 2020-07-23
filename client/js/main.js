438177995cf2c2092b52ab49c2e197e0ed667223
Luis Sousa <luispsousa@ua.pt>
2020-07-23 22:58:52 +0100

    Rating page added. Trying mobile menu animation

--- client/js/main.js
+++ client/js/main.js
@@ -1,12 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Handler when the DOM is fully loaded~
-  /* fetchStores(); */
+  
 });
 
-const fetchStores = (nProducts, sortBy) => {
+/* const fetchStores = (nProducts, sortBy) => {
   fetch(`http://localhost:5000/api/getStoresList?nStores=100&sortBy=name`)
     .then((response) => response.json())
     .then(({ data }) => {
       console.log(data);
     });
-};
+}; */
+const menu = document.getElementsByClassName('mobile-nav')[0];
+const openMenu = (icon) => {
+  if (icon.classList.contains('open')) {
+    icon.children[0].src = "/images/icons/menu.svg";
+    icon.classList.remove('open');
+    menu.classList.remove('show');
+  } else {
+    icon.children[0].src = "/images/icons/close.svg";
+    icon.classList.add('open');
+    menu.classList.add('show');
+  }
+}
